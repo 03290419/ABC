94ecd1d10e1def38221cb812e9c3738720d36976
hyunjin <alfma123@naver.com>
2024-01-13 20:02:26 +0900

    refactor: 쿼리 키 누락 에러처리 추가

--- src/controllers/allProductController.js
+++ src/controllers/allProductController.js
@@ -1,25 +1,27 @@
 const { allProductService } = require('../services');
+const { throwError } = require('../utils');
 const { productSortService, categoryCheckService } = allProductService;
 
 const allProduct = async (req, res) => {
   try {
-    const { category_id, sub_category_id, sortBy, page } = req.query; //  에러 핸들링 ----------------------------------------
-
+    const { categoryId, subCategoryId, sortBy, page } = req.query;
+    if (!categoryId || !page || !sortBy)
+      throwError(400, 'query key categoryId, page, sortBy are required');
     const categoryCheckController = await categoryCheckService(
-      sub_category_id,
-      category_id,
+      subCategoryId,
+      categoryId,
     );
 
-    if (categoryCheckController[0].parent_id != category_id) {
+    if (categoryCheckController[0].parent_id != categoryId) {
       const error = new Error();
       error.statusCode = 400;
       error.message = `category check result - main and sub category unmatched`;
       throw error;
-    } //  정렬 ----------------------------------------
+    }
 
     const productSortServiceResult = await productSortService(
-      sub_category_id,
-      category_id,
+      subCategoryId,
+      categoryId,
       sortBy,
       page,
     );
